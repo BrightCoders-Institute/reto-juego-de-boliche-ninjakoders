393c786f4c68eb1519da257f9b47378d8537d916
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-06-06 18:16:37 -0600

    Metodo para hacer los tiros de bolos aleatorios Co-Authored-By: OrlandoLeso <44180680+landillo@users.noreply.github.com> Co-Authored-By: Miguel Cobian Meraz <112898464+mcmmiguel@users.noreply.github.com> Co-authored-by: Iván Caetano<IvanCa27@users.noreply.github.com>

--- src/juegoDeBolos.js
+++ src/juegoDeBolos.js
@@ -4,3 +4,17 @@ const puntajeFrames = []; //guardamos la suma de los tiros de cada frame
 const tiro1 = []; // valor de tiro1
 const tiro2 = [];// valor tiro 2
 
+const tiros = () => {
+    for (let i = 0; i < 10; i++) {
+        tiro1[i] = Math.round(Math.random() * 10 );
+        const numero = 10 - tiro1[i];
+        tiro2[i] = Math.round(Math.random() * numero );
+
+        totalesTurnos[i] = tiro1[i] + tiro2[i];
+
+        const puntosTiro1 = document.getElementById(`tiro1_i${i.toString()}`);
+        puntosTiro1.innerHTML = tiro1[i];
+        const puntosTiro2 = document.getElementById(`tiro2_i${i.toString()}`);
+        puntosTiro2.innerHTML = tiro2[i];
+    }
+}
