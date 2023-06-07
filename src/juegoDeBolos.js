69d78135d7478c501fe00f028b9a6a532a948c37
Miguel Cobian <miguelcobian99@gmail.com>
2023-06-06 18:12:05 -0600

    Declaracion de variables globales
    Co-Authored-By: AlbertoG22 <47892591+AlbertoG22@users.noreply.github.com>
    Co-Authored-By: OrlandoLeso <44180680+landillo@users.noreply.github.com>
    Co-Authored-By: Miguel Cobian Meraz <112898464+mcmmiguel@users.noreply.github.com>

---
+++ src/juegoDeBolos.js
@@ -0,0 +1,6 @@
+const totalesTurnos = []; // guardamos el total de cada turno
+let sumaPuntos = 0; //suma de puntaje
+const puntajeFrames = []; //guardamos la suma de los tiros de cada frame
+const tiro1 = []; // valor de tiro1
+const tiro2 = [];// valor tiro 2
+
